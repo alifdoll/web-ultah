ef37efa960db7184a666833f2bbee4ea78e94974
alif <alifianfajarpratama@gmail.com>
2021-01-28 12:32:57 +0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="Source/IndexStyle.css">
+        <link rel="stylesheet" href="Source/ResponsiveIndex.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h2 id="weird-one">21 tahun</h2>      
+                <h2 id="weird-two">21 tahun</h2>
+            </div>
+        </header>
+        <main>
+            <div class="name-chara">
+                <h1>Febi Komaril</h1>
+            </div>
+            <div class="social-media">
+                <div class="logo plus">
+                    <a href="#" class="btn"><span class="fa fa-twitter"></span>twitter</a>
+                </div>
+                <div class="logo plus">
+                    <a href="#" class="btn"><span class="fa fa-instagram"></span>instagram</a>
+                </div>
+                <div class="logo">
+                    <a href="#" class="btn"><span class="fa fa-video-camera"></span>video</a>
+                </div>
+            </div>
+            <div class="image-chara">
+                <img src="Asset/Osinya Azriel.png">
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>

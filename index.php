5627ca10a0cf028609b272fe1d1cf474683003c9
unknown <armandodiazer@gmail.com>
2021-02-03 14:23:46 +0700

    UPDATE RESPONSIVE

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Journey</title>
+        <title>Endless Journey</title>
         <link rel="shortcut icon" type="image" href="Asset/icon.jpg">
         <link rel="stylesheet" href="Source/IndexStyle.css">
         <link rel="stylesheet" href="Source/ResponsiveIndex.css">
@@ -12,34 +12,38 @@
     <body>
         <header>
             <div class="container">
-                <img src="Asset/ej.png">
+                <div class="respo">
+                    <img src="Asset/ej.png">
+                </div>
+                <div class="name-chara">
+                    <h1>Febi Komaril</h1>
+                </div>
+                <div class="social-media">
+                    <div class="logo">
+                        <a href="https://twitter.com/Febinetsui_ID/status/1354674289790787586?s=20" class="btn" target="_blank">Twitter</a>
+                    </div>
+                    <div class="logo">
+                        <a href="https://open.spotify.com/episode/1AJ2LGHrJLnl8Dm3zu1TSv?si=4RfO_tYSS_aagVjq40HzMw" class="btn" target="_blank">Podcast</a>
+                    </div>
+                </div>
+                <div class="image-chara">
+                    <img src="Asset/Osinya Azriel.png">
+                </div>
             </div>
         </header>
-        <main>
-            <div class="name-chara">
-                <h1>Febi Komaril</h1>
+        <div class="top-wrapper">
+            <video autoplay loop muted>
+                <source src="Asset/kafebi_azriel.mp4" type="video/mp4">
+            </video>
+            <div class="top-wrappers">
+                <a href="https://youtu.be/y8-Rm9IVjYA" class="btn-new hadiah" target="_blank">Hadiah</a>
             </div>
-            <div class="social-media">
-                <div class="logo plus">
-                    <a href="https://twitter.com/Febinetsui_ID/status/1354674289790787586?s=20" class="btn">Twitter</a>
-                </div>
-                <div class="logo plus">
-                    <a href="#" class="btn">Book</a>
-                </div>
-                <div class="logo">
-                    <a href="#" class="btn">Podcast</a>
-                </div>
-            </div>
-            <div class="image-chara">
-                <img src="Asset/Osinya Azriel.png">
-            </div>
-            <div class="top-wrapper">
-                <video autoplay loop muted>
-                    <source src="Asset/kafebi_azriel.mp4" type="video/mp4">
-                </video>
-            </div>
-        </main>
+        </div>
         <footer>
+            <div class="container">
+                <h1>ATIGA<span>.</span></h1>
+                <p>@2021 Powered by ATIGA Team</p>
+            </div>
         </footer>
     </body>
 </html>
